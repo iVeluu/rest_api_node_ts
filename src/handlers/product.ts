ce2d79ee8500f85b56d51eeaa777a6359d393bc6
iVeluu <velualex10@gmail.com>
2024-06-25 12:12:40 -0600

    get, getbyId, create

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,8 +1,38 @@
-import { Request, Response } from "express"
+import e, { Request, Response } from "express"
 import Product from "../models/Product.models"
+import { validationResult } from "express-validator"
+
+export const getProducts = async ( req : Request, res : Response ) => {
+    try {
+        const products = await Product.findAll()
+        res.json({data: products})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getProductById = async ( req : Request, res : Response ) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findByPk(id)
+        
+        if(!product){
+            return res.status(404).json({
+                error: 'Producto No Encontrado'
+            })
+        }
+        
+        res.json({data: product})
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 export const createProduct = async ( req : Request, res : Response ) => {
-    const product = await Product.create(req.body)
-
-    res.json({data: product})
+    try {
+        const product = await Product.create(req.body)
+        res.json({data: product})
+    } catch (error) {
+        console.log(error)
+    }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,29 @@
 import { Router } from "express";
-import { createProduct } from "./handlers/product";
+import { createProduct, getProducts, getProductById } from "./handlers/product";
+import { body, param } from "express-validator";
+import { handleInputErrors } from "./middleware";
 
 const router = Router();
 
 //Routing
-router.get('/', ( req, res) => {
-    res.json('Desde GET')
-}) 
+router.get('/', getProducts) 
+router.get('/:id', 
 
-router.post('/', createProduct) 
+    param('id').isInt().withMessage('ID no válido'),
+    handleInputErrors,
+    getProductById
+) 
+
+router.post('/',
+   body('name')
+        .notEmpty().withMessage('El nombre del producto no puede ir vacio'),
+   body('price')
+        .isNumeric().withMessage('Valor no válido')
+        .notEmpty().withMessage('El precio del producto no puede ir vacio')
+        .custom( value => value > 0).withMessage('Valor no válido'),
+    handleInputErrors,
+    createProduct
+) 
 
 
 export default router
