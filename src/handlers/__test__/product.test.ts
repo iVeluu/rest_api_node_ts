de776499a3bdb56616c585085fa990cea9fb9474
iVeluu <velualex10@gmail.com>
2024-06-26 16:32:00 -0600

    test for POST produts / script for pretest

--- src/handlers/__test__/product.test.ts
+++ src/handlers/__test__/product.test.ts
@@ -1,4 +1,56 @@
 import  request  from "supertest";
 import server from "../../server";
 
+describe('POST /api/products', () => {
 
+    it('should display validation errors', async () => {
+        const response = await request(server).post('/api/products').send({})
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+        
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+
+    it('should validate that the price is greater than 0', async () => {
+        const response = await request(server).post('/api/products').send({
+            name: 'Monitor curvo - testing',
+            price: 0
+        })
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+        
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+
+    it('should validate that the price is a number and greater than 0', async () => {
+        const response = await request(server).post('/api/products').send({
+            name: 'Monitor curvo - testing',
+            price: 'hola'
+        })
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(2)
+        
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(4)
+    })
+
+
+    it('should create a new product', async () => {
+        const response = await request(server).post('/api/products').send({
+            name: "Desde Testing",
+            price: 300
+        })
+        expect(response.status).toBe(201)
+        expect(response.body).toHaveProperty('data')
+        
+        expect(response.status).not.toBe(400)
+        expect(response.status).not.toBe(200)
+        expect(response.body).not.toHaveProperty('errors')
+
+    })
+})
