30931964e24ab9e0c7e4b0b78c85777c8ea71d92
iVeluu <velualex10@gmail.com>
2024-06-26 17:36:18 -0600

    Testing method GET

--- src/handlers/__test__/product.test.ts
+++ src/handlers/__test__/product.test.ts
@@ -54,3 +54,48 @@ describe('POST /api/products', () => {
 
     })
 })
+
+describe('GET /api/products', () => {
+    it('should check if api/product url exists', async () => {
+        const response = await request(server).get('/api/products')
+        expect(response.status).not.toBe(404)
+    })
+
+    it('get a json response with the products', async () => {
+        const response = await request(server).get('/api/products')
+
+        expect(response.status).toBe(200)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.body).toHaveProperty('data')
+        expect(response.body.data).toHaveLength(1)
+        expect(response.body).not.toHaveProperty('errros')
+
+    })
+})
+
+describe('GET /api/products/:id', () => {
+    it('should return a 404 response for a non-existent product', async () => {
+        const productId = 2000
+        const response = await request(server).get(`/api/products/${productId}`)
+
+        expect(response.status).toBe(404)
+        expect(response.body).toHaveProperty('error')
+        expect(response.body.error).toBe('Producto No Encontrado')
+    })
+
+    it('should check a valid ID in the URL', async () => {
+
+        const response = await request(server).get('/api/products/not-valid-url')
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+        expect(response.body.errors[0].msg).toBe('ID no válido')
+    })
+
+    it('get a json with a single product', async () => {
+        const response = await request(server).get('/api/products/1')
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty('data')
+    })
+})
